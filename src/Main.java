72528e64139e0c43f84676de40e3f4f603f825ec
Talal Habbal <talal-habbal@hotmail.com>
2023-12-07 16:24:48 +0100

    Add Digital Signature

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.KeyStore;
-import java.security.PrivateKey;
 import java.util.Arrays;
 import javax.crypto.Cipher;
 import javax.crypto.Mac;
@@ -14,19 +12,22 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
+import java.security.cert.CertificateFactory;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import javax.crypto.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            // Task 1: Split the content of ciphertext.enc
+            //Split the content of ciphertext.enc
             byte[] ciphertext = Files.readAllBytes(Paths.get("C:\\Users\\talal\\Desktop\\Security Lab\\resources\\ciphertext.enc"));
             byte[] encryptedSymmetricKey = Arrays.copyOfRange(ciphertext, 0, 128);
             byte[] encryptedIV = Arrays.copyOfRange(ciphertext, 128, 256);
             byte[] encryptedHmacKey = Arrays.copyOfRange(ciphertext, 256, 384);
             byte[] encryptedData = Arrays.copyOfRange(ciphertext, 384, ciphertext.length);
 
-            // Task 2: Decrypt using the private key from the keystore
+            //Decrypt using the private key from the keystore
             KeyStore keystore = KeyStore.getInstance("JKS");
             keystore.load(new FileInputStream("C:\\Users\\talal\\Desktop\\Security Lab\\resources\\lab1Store"), "lab1StorePass".toCharArray());
             PrivateKey privateKey = (PrivateKey) keystore.getKey("lab1EncKeys", "lab1KeyPass".toCharArray());
@@ -36,14 +37,14 @@ public class Main {
             byte[] symmetricKey = rsaCipher.doFinal(encryptedSymmetricKey);
             byte[] iv = rsaCipher.doFinal(encryptedIV);
 
-            // Task 3: Decrypt the data using the decrypted key and IV
+            //Decrypt the data using the decrypted key and IV
             SecretKeySpec secretKeySpec = new SecretKeySpec(symmetricKey, "AES");
             IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
             Cipher aesCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             aesCipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
             byte[] decryptedData = aesCipher.doFinal(encryptedData);
 
-            // Task 4: Verify integrity using Message Authentication Code (HmacMD5)
+            //Verify integrity using Message Authentication Code (HmacMD5)
             byte[] hmacKey = rsaCipher.doFinal(encryptedHmacKey);
             String givenHMAC = Files.readString(Paths.get("C:\\Users\\talal\\Desktop\\Security Lab\\resources\\ciphertext.mac2.txt"));
             SecretKeySpec hmacKeySpec = new SecretKeySpec(hmacKey, "HmacMD5");
@@ -52,20 +53,32 @@ public class Main {
             byte[] computedHmac = hmac.doFinal(decryptedData);
             String computedHMACtoHex = bytesToHex(computedHmac);
             boolean integrityVerified = computedHMACtoHex.equals(givenHMAC);
+            boolean isVerified = verification();
 
-            // Additional code to use the decrypted data as needed
+            
             String plaintext = new String(decryptedData, StandardCharsets.UTF_8);
             System.out.println("Decrypted Message: " + plaintext);
             System.out.println("Integrity Verified: " + integrityVerified);
-            
-            System.out.println("Given HMAC: " + givenHMAC);
-            System.out.println("Computed HMAC: " + computedHMACtoHex);
+            System.out.println("Digital Signature Verified: " + isVerified);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public static boolean verification() {
+        try {
+            FileInputStream readPublicKey = new FileInputStream("C:\\Users\\talal\\Desktop\\Security Lab\\resources\\lab1Sign.cert");
+            CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate x509Certificate = (X509Certificate) certificateFactory.generateCertificate(readPublicKey);
+            PublicKey publickey = x509Certificate.getPublicKey();
+            x509Certificate.verify(publickey);
+            return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
     public static String bytesToHex(byte[] bytes) {
     StringBuilder hexString = new StringBuilder(2 * bytes.length);
 
